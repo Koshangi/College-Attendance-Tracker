69a24610e178900eb881841acee1349bba3ab88e
Koshangi <161786269+Koshangi@users.noreply.github.com>
2025-01-20 17:40:44 +0530

    Delete html/html directory

--- html/html/html/setup_backend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Setting up the backend..."
-
-# Install Node.js dependencies
-npm install express sqlite3 body-parser cors
-
-echo "Starting the backend server on localhost:3031..."
-node backend.js
